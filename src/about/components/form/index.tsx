5dee819ea883e27252a298c0f96f3c5f92b7db04
Dillon Jason <djasuar@gmail.com>
2019-02-03 14:40:38 -0500

    Created form input component

--- src/about/components/form/index.tsx
+++ src/about/components/form/index.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react'
 
+import { Input } from '../input'
+
 import * as styles from './styles.scss'
 
-export const Form = () => <div className={styles.container}>Form</div>
+export const Form = () => (
+  <div className={styles.container}>
+    <Input
+      id='name'
+      label='Name'
+      textArea
+      onChange={({ value }) => console.log(value)}
+    />
+  </div>
+)
---
+++ src/about/components/input/index.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react'
+
+import * as styles from './styles.scss'
+
+interface onChangeParams {
+  value: string
+}
+
+interface InputProps {
+  onChange(params: onChangeParams): void
+  id: string
+  label: string
+  textArea?: boolean
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>
+  textAreaProps?: React.InputHTMLAttributes<HTMLTextAreaElement>
+}
+
+interface InputState {
+  value: string
+  noInput: boolean
+}
+
+export class Input extends React.Component<InputProps, InputState> {
+  constructor(props: InputProps) {
+    super(props)
+    this.state = {
+      value: '',
+      noInput: true
+    }
+
+    this.onInputChange = this.onInputChange.bind(this)
+    this.onTextAreaChange = this.onTextAreaChange.bind(this)
+    this.onFocus = this.onFocus.bind(this)
+    this.onBlur = this.onBlur.bind(this)
+    this.getLabelPositionClass = this.getLabelPositionClass.bind(this)
+  }
+
+  onInputChange(event: React.FormEvent<HTMLInputElement>): void {
+    const value = event.currentTarget.value
+    this.setState({ value })
+    this.props.onChange({ value })
+  }
+
+  onTextAreaChange(event: React.FormEvent<HTMLTextAreaElement>): void {
+    const value = event.currentTarget.value
+    this.setState({ value })
+    this.props.onChange({ value })
+  }
+
+  onFocus(): void {
+    this.setState({ noInput: false })
+  }
+
+  onBlur(): void {
+    this.setState({ noInput: !Boolean(this.state.value) })
+  }
+
+  getLabelPositionClass(): string {
+    return this.state.noInput ? styles.noInput : null
+  }
+
+  setInputFocus(): void {}
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <label className={this.getLabelPositionClass()} htmlFor={this.props.id}>
+          {this.props.label}
+        </label>
+        {this.props.textArea ? (
+          <textarea
+            id={this.props.id}
+            className={styles.input}
+            name={this.props.id}
+            onChange={this.onTextAreaChange}
+            onFocus={this.onFocus}
+            onBlur={this.onBlur}
+            {...this.props.textAreaProps}
+          />
+        ) : (
+          <input
+            id={this.props.id}
+            className={styles.input}
+            name={this.props.id}
+            onChange={this.onInputChange}
+            onFocus={this.onFocus}
+            onBlur={this.onBlur}
+            {...this.props.inputProps}
+          />
+        )}
+      </div>
+    )
+  }
+}
